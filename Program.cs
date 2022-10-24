043766ca88b485d5307d218b8fafd6c489f356a4
KyleBax <102880750+KyleBax@users.noreply.github.com>
2022-10-24 17:36:26 +1300

    removed method that writes out random numbers, created new method that returns the random number so it can be used elsewhere. Made a method to adjust the totalAmount and output that you won.

--- Program.cs
+++ Program.cs
@@ -4,48 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("INSERT COINS");
             int totalAmount = 100;
+
             while (totalAmount > 0)
             {
                 Console.WriteLine("available balance");
                 Console.WriteLine(totalAmount);
                 Console.WriteLine("How much would you like to bet?");
-                int bettingAmount = 0;
-                bettingAmount = Convert.ToInt32(Console.ReadLine());
+                int bettingAmount = Convert.ToInt32(Console.ReadLine());
                 while (bettingAmount <= 0)
                 {
                     Console.WriteLine("You must place a valid bet");
                     Console.WriteLine("How much would you like to bet?");
                     bettingAmount = Convert.ToInt32(Console.ReadLine());
                 }
-
-                Console.WriteLine("How many lines are you betting on?");
-                Console.WriteLine("1 3 5");
-                int linesBet = Convert.ToInt32(Console.ReadLine());
-                bettingAmount *= linesBet;
                 if (bettingAmount > totalAmount)
                 {
-                    Console.WriteLine("You do not have that much to bet");
+                    Console.WriteLine("You have bet all your remainning credit");
+                    bettingAmount = totalAmount;
                 }
-
-                PullTheLever(bettingAmount, linesBet);
-                totalAmount -= bettingAmount;
-            }
-        }
-
-        public static void PullTheLever(int bettingAmount, int linesBet)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int a = 0; a < 3; a++)
+                int ranNumOne = GetRandomNumber();
+                int ranNumTwo = GetRandomNumber();
+                int ranNumThree = GetRandomNumber();
+                Console.WriteLine(ranNumOne + " " + ranNumTwo + " " + ranNumThree);
+                if (ranNumOne == ranNumTwo || ranNumTwo == ranNumThree)
                 {
-                    Random random = new Random();
-                    int ranNum = random.Next(0, 8);
-                    Console.Write(ranNum + " ");
+                    //TODO remove magic number win mmodifiers
+                    totalAmount = Win(bettingAmount, totalAmount, 1);
                 }
-                Console.WriteLine();
+                else if (ranNumOne == ranNumTwo && ranNumTwo == ranNumThree)
+                {
+                    if (ranNumOne == 7)
+                    {
+                        totalAmount = Win(bettingAmount, totalAmount, 7);
+                    }
+                    else
+                    {
+                        totalAmount = Win(bettingAmount, totalAmount, 2);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You Lose!!!");
+                    totalAmount -= bettingAmount;
+                }
             }
+            Console.WriteLine("looks like you ran out of credit!!");
+        }
+        public static int GetRandomNumber()
+        {
+            Random random = new();
+            int ranNum = random.Next(0, 8);
+            return ranNum;
+        }
+        public static int Win(int bettingAmount, int totalAmount, int winModifier)
+        {
+            Console.WriteLine("Winner!!!!");
+            totalAmount -= bettingAmount;
+            int winningAmount = bettingAmount * winModifier;
+            Console.WriteLine("You win " + winningAmount + " credits");
+            Console.WriteLine(totalAmount);
+            totalAmount += winningAmount;
+            Console.WriteLine(totalAmount);
+            return totalAmount;
         }
     }
 }
